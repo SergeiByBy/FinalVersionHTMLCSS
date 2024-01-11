51b4444116c85b9057fd5b60724cc14303c3eb08
Sergei <mizuno338@mail.ru>
2024-01-11 16:06:56 +0300

    finalVersion

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// cliclable.onclick = function() {
+//     alert('Произошёл  yt sdfdsfsdfsdсбой!');
+
+// }
+
+// button.addEventListener("click", function() {
+//     alert('Произошёл  ');
+
+// })
+// buttonNew.addEventListener("mousemove", function() {
+//     alert('Произошёл  мышка!');
+
+// })
+
+function btnBlue(e) {
+    btnBlue.style.color = e.blue;
+
+}
+body.addAventListener("click", btnBlue);
+// function moveBlock(e) {
+//     block.style.left = e.pageX + 'px';
+//     block.style.top = e.pageY + 'px';
+//     block.style.backgroundColor = `rgb( ${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)})`;
+// }
+// body.addEventListener("click", moveBlock);
